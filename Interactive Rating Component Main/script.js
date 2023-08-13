2d2d7acb8a400f3b7404fcdc0f8830192bd99da6
Christopher J <chrisbjones1987@yahoo.com>
2023-08-13 15:11:50 -0500

    Added Script.js File to Interactive Rating Project

---
+++ Interactive Rating Component Main/script.js
@@ -0,0 +1,6 @@
+// i need to be able to select the rating from the rating card
+// i need to be able to click the submit button
+// i need to be able to carry the rating from the rating card and display it on the thank you card
+// i need to hide the rating card once the submit button is clicked on the rating card
+// i need to diplay the thank you card when the submit button is clicked on the rating card
+
