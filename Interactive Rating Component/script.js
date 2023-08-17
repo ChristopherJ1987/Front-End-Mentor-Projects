55cd2179c9468f9e8706457668f40c9775ec0d31
Christopher J <chrisbjones1987@yahoo.com>
2023-08-17 15:59:27 -0500

    Updated Interactive Rating Project

--- Interactive Rating Component/script.js
+++ Interactive Rating Component/script.js
@@ -4,3 +4,16 @@
 // i need to hide the rating card once the submit button is clicked on the rating card
 // i need to diplay the thank you card when the submit button is clicked on the rating card
 
+const form = document.querySelector(".form");
+const template = document.querySelector(".thankYouCardHolder");
+const wrapper = document.querySelector(".ratingCardHolder");
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const rating = new FormData(event.target).get("rating");
+    if(rating) {
+        wrapper.innerHTML = template.innerHTML.replace(/{{ rating }}/, rating);
+    }
+}
+
+form.addEventListener("submit", handleSubmit);
